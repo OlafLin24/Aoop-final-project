e34be7a158ec41ae9fea08135592a8aee6e34373
charlie-ww <smallforest0819@gmail.com>
2025-01-03 14:43:22 +0800

    feat: add image to AddIcon class
    - Load and scale the image "jerry.png" in the AddIcon constructor
    - Replace the circle and lines drawing with the image in the draw method
    - Comment out the previous drawing code for the circle and lines

--- add_icon.py
+++ add_icon.py
@@ -12,13 +12,19 @@ class AddIcon:
         self.y = y
         self.radius = 15  # 加號球的大小
         self.color = GREEN
+        self.image = pygame.image.load("images/jerry.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 30))
 
     def draw(self, screen):
         """繪製加號球"""
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+        #pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
         # 繪製加號標誌
-        pygame.draw.line(screen, WHITE, (self.x - 5, self.y), (self.x + 5, self.y), 2)
-        pygame.draw.line(screen, WHITE, (self.x, self.y - 5), (self.x, self.y + 5), 2)
+        #pygame.draw.line(screen, WHITE, (self.x - 5, self.y), (self.x + 5, self.y), 2)
+        #pygame.draw.line(screen, WHITE, (self.x, self.y - 5), (self.x, self.y + 5), 2)
+        # from rat.jpg
+        #add_icon = pygame.image.load("images/rat.jpg")
+        screen.blit(self.image, (self.x-15, self.y-15))
+
 
     def check_collision(self, ball):
         """檢查與小球的碰撞"""
